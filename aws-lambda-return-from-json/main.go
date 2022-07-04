ddd645108f9e2b9a5461d6d4c74bfdb0e05c71f2
anderson <envieparaanderson@gmail.com>
2022-07-04 06:50:30 -0300

    (Em andamento) Implementando a busca dentro do json

--- aws-lambda-return-from-json/main.go
+++ aws-lambda-return-from-json/main.go
@@ -15,7 +15,7 @@ func main() {
     if err != nil {
         fmt.Println(err)
     }
-    fmt.Println("Successfully Opened users.json")
+    fmt.Println("Successfully Opened links.json")
     // defer the closing of our jsonFile so that we can parse it later on
     defer jsonFile.Close()
 
