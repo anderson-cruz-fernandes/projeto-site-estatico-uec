ab9d8f0a5c0fbd4cdba127989813378eefcd87a3
anderson <envieparaanderson@gmail.com>
2022-07-10 09:58:15 -0300

    Embutido o json dentro do  compilado Não faz mais a leitura diretamente do filesystem

--- aws-lambda-return-from-json/main.go
+++ aws-lambda-return-from-json/main.go
@@ -1,52 +1,63 @@
 package main
 
 import (
+	_ "embed"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
 )
 
+/*func main() {
+	lambda.Start(handler)
+}*/
+
+/*func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+
+switch req.HTTPMethod {
+case "GET":
+	return
+}*/
+
+//start := time.Now()
+// Open our jsonFile
+
+/*jsonFile, err := os.Open("links.json")
+// if we os.Open returns an error then handle it
+if err != nil {
+	fmt.Println(err)
+}
+fmt.Println("Successfully Opened links.json")
+// defer the closing of our jsonFile so that we can parse it later on
+defer jsonFile.Close()*/
+
+type usuario struct {
+	Nome   string
+	Turma  string
+	Link   string
+	Pedido string
+	Hash   string
+}
+
+//go:embed links.json
+var usuariosFile []byte
+var usuariosFunc = func() (usuarios []usuario) {
+	if err := json.Unmarshal(usuariosFile, &usuarios); err != nil {
+		panic(err)
+	}
+	return
+}()
+
+//byteValue, _ := ioutil.ReadAll(jsonFile)
 func main() {
+	//var usuarios []Usuario
+	var usuarios = usuariosFunc
 
-	// Open our jsonFile
-	jsonFile, err := os.Open("links.json")
-	// if we os.Open returns an error then handle it
+	//fmt.Printf("%+v\n", usuariosFunc)
+	//fmt.Println("tipo: \n", reflect.TypeOf(usuariosFunc))
+
+	/*err = json.Unmarshal(byteValue, &usuarios)
 	if err != nil {
 		fmt.Println(err)
-	}
-	fmt.Println("Successfully Opened links.json")
-	// defer the closing of our jsonFile so that we can parse it later on
-	defer jsonFile.Close()
-
-	/*	type Usuario struct {
-		Nome   string `json:"nome"`
-		Turma  string `json:"turma"`
-		Link   string `json:"link"`
-		Pedido string `json:"pedido"`
-		Hash   string `json:"hash"`
 	}*/
-	type Usuario struct {
-		Nome   string
-		Turma  string
-		Link   string
-		Pedido string
-		Hash   string
-	}
-
-	/*type Usuarios struct {
-		//Usuarios []Usuario `json:"usuarios"`
-		Usuarios []Usuario `json:""`
-	}*/
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	var usuarios []Usuario
-
-	err = json.Unmarshal(byteValue, &usuarios)
-	if err != nil {
-		fmt.Println(err)
-	}
 
 	busca := "A03CJL9T"
 	resultado := "0"
@@ -61,6 +72,8 @@ func main() {
 	}
 
 	fmt.Println(resultado)
+	//elapsed := time.Since(start)
+	//fmt.Printf("Executou em %s", elapsed)
 
 	//OU se for muito usada usar a de baixo
 	//Also if this is a frequent operation
@@ -74,6 +87,8 @@ func main() {
 	if v, ok := confMap[x]; ok {
 		fmt.Println(v)
 	}*/
-
 	//#############################
+	//https://stackoverflow.com/a/38654444
+	//http://www.inanzzz.com/index.php/post/1rwm/including-and-reading-static-files-with-embed-directive-at-compile-time-in-golang
+
 }
