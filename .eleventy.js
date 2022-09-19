bcdad6711e3878194b854652ee11dd61931a1bea
anderson-cruz-fernandes <anderson-cruz-fernandes@users.noreply.github.com>
2022-09-19 03:25:22 +0000

    Implementação do CMS (Em andamento)

--- .eleventy.js
+++ .eleventy.js
@@ -74,6 +74,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/_redirects');
   eleventyConfig.addPassthroughCopy('src/_headers');
+  eleventyConfig.addPassthroughCopy('src/admin');
   
   return {
     dir: {
