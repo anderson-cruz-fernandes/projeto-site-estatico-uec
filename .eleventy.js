8c02c09943fa9d271422287f9bd55a985d16b8e4
anderson-cruz-fernandes <anderson-cruz-fernandes@users.noreply.github.com>
2022-12-26 10:25:10 +0000

    Ajustes nos arquivos de redirect

--- .eleventy.js
+++ .eleventy.js
@@ -76,7 +76,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/_headers');
   eleventyConfig.addPassthroughCopy('src/admin');
   eleventyConfig.addPassthroughCopy('src/feed');
-  eleventyConfig.addPassthroughCopy('src/external_redirect/*.html');
   
   return {
     dir: {
